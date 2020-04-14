2a44ed5f8df5b20e2ef7ce68f910a3f45c02c13a
RamziBach <ramzibach.dev@gmail.com>
2020-04-13 20:17:16 -0400

    Advice generator project
    Small project

---
+++ script.js
@@ -0,0 +1,24 @@
+const advice = document.querySelector('.advice');
+const btn = document.querySelector('.btn');
+
+// Fetch API
+
+//fetch('https://api.adviceslip.com/advice')
+//	.then(res => {
+//		if (!res.ok) throw new Error('Error');
+//		return res.json();
+//	})
+//	.then(data => advice.innerHTML = data.slip.advice)
+//	.catch(err => console.error(err));
+
+// async/await
+async function getAdvice() {
+	const response = await fetch('https://api.adviceslip.com/advice');
+	const data = await response.json();
+	advice.innerHTML = `"${data.slip.advice}"`;
+}
+
+btn.addEventListener('click', () => {
+	getAdvice()
+		.catch(err => console.error('Error'));
+});
