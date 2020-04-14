1806c80e3176639bb943194e4ceba59d27b01041
RamziBach <ramzibach.dev@gmail.com>
2020-04-14 11:43:31 -0400

    added try/catch

--- script.js
+++ script.js
@@ -12,13 +12,30 @@ const btn = document.querySelector('.btn');
 //	.catch(err => console.error(err));
 
 // async/await
-async function getAdvice() {
-	const response = await fetch('https://api.adviceslip.com/advice');
-	const data = await response.json();
-	advice.innerHTML = `"${data.slip.advice}"`;
-}
+
+//async function getAdvice() {
+//	const response = await fetch('https://api.adviceslip.com/advice');
+//	const data = await response.json();
+//	advice.innerHTML = `"${data.slip.advice}"`;
+//}
+//
+//btn.addEventListener('click', () => {
+//	getAdvice()
+//		.catch(err => console.error('Error'));
+//});
+
+const getAdvice = async () => {
+	try {
+		const response = await fetch('https://api.adviceslip.com/advice');
+		if (response.ok) {
+			const jsonResponse = await response.json();
+			advice.innerHTML = `"${jsonResponse.slip.advice}"`;
+		}
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 btn.addEventListener('click', () => {
-	getAdvice()
-		.catch(err => console.error('Error'));
+	getAdvice();
 });
